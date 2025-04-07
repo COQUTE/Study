7a6ffd7d0f76ee9c33ddea15e855fe56414373d6
SemK0205 <s413625@gamil.com>
2025-04-07 10:24:46 +0900

    더미파일 삭제

--- SejongProject/src/main/java/com/study/controllers/printGradeController.java
+++ SejongProject/src/main/java/com/study/controllers/printGradeController.java
@@ -20,7 +20,7 @@ public class printGradeController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String stdId = req.getParameter("stdId");
+		
 		
 		SejongService service = new SejongService();
 		
